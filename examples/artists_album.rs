18b06ef793c775cd78facfc6ad27fda78f489552
Samray Leung <samrayleung@gmail.com>
2018-01-22 23:29:12 +0800

    add examples

---
+++ examples/artists_album.rs
@@ -0,0 +1,24 @@
+extern crate rspotify;
+
+use rspotify::spotify::client::Spotify;
+use rspotify::spotify::oauth2::SpotifyClientCredentials;
+use rspotify::spotify::spotify_enum::ALBUM_TYPE;
+
+fn main() {
+    // Set client_id and client_secret in .env file or
+    // export CLIENT_ID="your client_id"
+    // export CLIENT_SECRET="secret"
+    let client_credential = SpotifyClientCredentials::default().build();
+
+    // Or set client_id and client_secret explictly
+    // let client_credential = SpotifyClientCredentials::default()
+    //     .client_id("this-is-my-client-id")
+    //     .client_secret("this-is-my-client-secret")
+    //     .build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+    let mut birdy_uri = String::from("spotify:artist:2WX2uTcsvV5OnS0inACecP");
+    let albums = spotify.artist_albums(&mut birdy_uri, Some(ALBUM_TYPE::ALBUM), None, None, None);
+    println!("{:?}", albums.unwrap());
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,48 @@
-struct Spotify{
-    prefix:String,
-    
-}
+#![allow(dead_code)]
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+extern crate chrono;
+#[macro_use]
+extern crate serde_derive;
+extern crate webbrowser;
+
+extern crate dotenv;
+use serde_json::Error;
+extern crate url;
+extern crate percent_encoding;
+extern crate rand;
+extern crate base64;
+#[macro_use]
+extern crate hyper;
+
+#[macro_use]
+extern crate derive_builder;
+use percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
+use rocket::response::Redirect;
+use reqwest::header::{Authorization, Basic, Bearer};
+use reqwest::Client;
+use rocket_contrib::{Json, Value};
+// use rand::Rng;
+
+use dotenv::dotenv;
+use std::env;
+
+use std::io::Read;
+use std::path::PathBuf;
+use std::collections::HashMap;
+pub mod spotify;
+// use spotify::client::Spotify;
+// use spotify::model::album_item::Item;
+// use spotify::oauth2::{TokenInfo, SpotifyOAuth, SpotifyClientCredentials};
+// use spotify::util::{generate_random_string, datetime_to_timestamp, prompt_for_user_token};
+// use spotify::spotify_enum::{ALBUM_TYPE, TYPE};
+
