3d642afeede3601fa8b70e6fd89d504ef18ccee2
Samray <samrayleung@gmail.com>
2018-02-07 23:05:43 +0800

    clear unused import

--- src/lib.rs
+++ src/lib.rs
@@ -23,24 +23,6 @@ extern crate base64;
 #[macro_use]
 extern crate hyper;
 
-// use percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
-// use rocket::response::Redirect;
-// use reqwest::header::{Authorization, Basic, Bearer};
-// use reqwest::Client;
-// use rocket_contrib::{Json, Value};
-// use rand::Rng;
-
-// use dotenv::dotenv;
-// use std::env;
-
-// use std::io::Read;
-// use std::path::PathBuf;
-// use std::collections::HashMap;
 pub mod spotify;
 mod errors;
 pub use errors::{Result, Error, ErrorKind};
-// use spotify::client::Spotify;
-// use spotify::model::album_item::Item;
-// use spotify::oauth2::{TokenInfo, SpotifyOAuth, SpotifyClientCredentials};
-// use spotify::util::{generate_random_string, datetime_to_timestamp, prompt_for_user_token};
-// use spotify::spotify_enum::{ALBUM_TYPE, TYPE};
