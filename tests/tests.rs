4b8a3e4752eb699435c2167d4f64f2a7d517c26f
Samray <samrayleung@gmail.com>
2018-02-19 16:32:39 +0800

    remove unnessary comments

--- tests/tests.rs
+++ tests/tests.rs
@@ -115,12 +115,6 @@ fn test_artist_top_tracks() {
 #[test]
 fn test_audio_analysis() {
     let client_credential = SpotifyClientCredentials::default().build();
-
-    // Or set client_id and client_secret explictly
-    // let client_credential = SpotifyClientCredentials::default()
-    //     .client_id("this-is-my-client-id")
-    //     .client_secret("this-is-my-client-secret")
-    //     .build();
     let spotify = Spotify::default()
         .client_credentials_manager(client_credential)
         .build();
@@ -183,11 +177,6 @@ fn test_current_playback() {
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
-            // Or set client_id and client_secret explictly
-            // let client_credential = SpotifyClientCredentials::default()
-            //     .client_id("this-is-my-client-id")
-            //     .client_secret("this-is-my-client-secret")
-            //     .build();
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
@@ -267,12 +256,6 @@ fn test_current_user_playlists() {
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
-
-            // Or set client_id and client_secret explictly
-            // let client_credential = SpotifyClientCredentials::default()
-            //     .client_id("this-is-my-client-id")
-            //     .client_secret("this-is-my-client-secret")
-            //     .build();
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
@@ -293,11 +276,6 @@ fn test_current_user_recently_played() {
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
-            // Or set client_id and client_secret explictly
-            // let client_credential = SpotifyClientCredentials::default()
-            //     .client_id("this-is-my-client-id")
-            //     .client_secret("this-is-my-client-secret")
-            //     .build();
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
@@ -380,16 +358,9 @@ fn test_current_user_saved_tracks_contains() {
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
-            // Or set client_id and client_secret explictly
-            // let client_credential = SpotifyClientCredentials::default()
-            //     .client_id("this-is-my-client-id")
-            //     .client_secret("this-is-my-client-secret")
-            //     .build();
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
-            //this is my(samray's) user_id and playlist_id, so just change
-            // user_id and playlist_id to yours, or you will get a 403 forbidden error
             let mut tracks_ids = vec![];
             let track_id1 = String::from("spotify:track:4iV5W9uYEdYUVa79Axb7Rh");
             let track_id2 = String::from("spotify:track:1301WleyT98MSxVHPZCA6M");
@@ -411,16 +382,9 @@ fn test_current_user_saved_tracks_delete() {
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
-            // Or set client_id and client_secret explictly
-            // let client_credential = SpotifyClientCredentials::default()
-            //     .client_id("this-is-my-client-id")
-            //     .client_secret("this-is-my-client-secret")
-            //     .build();
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
-            //this is my(samray's) user_id and playlist_id, so just change
-            // user_id and playlist_id to yours, or you will get a 403 forbidden error
             let mut tracks_ids = vec![];
             let track_id1 = String::from("spotify:track:4iV5W9uYEdYUVa79Axb7Rh");
             let track_id2 = String::from("spotify:track:1301WleyT98MSxVHPZCA6M");
@@ -1025,11 +989,6 @@ fn test_user_playlist_follow_playlist() {
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
-            // Or set client_id and client_secret explictly
-            // let client_credential = SpotifyClientCredentials::default()
-            //     .client_id("this-is-my-client-id")
-            //     .client_secret("this-is-my-client-secret")
-            //     .build();
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
