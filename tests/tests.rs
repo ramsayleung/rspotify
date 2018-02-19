3be45464e2d6de20608b152e1a1cd2bc92cbe068
Samray <samrayleung@gmail.com>
2018-02-19 10:58:32 +0800

    add test about artist

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,6 +2,8 @@ extern crate rspotify;
 
 use rspotify::spotify::client::Spotify;
 use rspotify::spotify::oauth2::SpotifyClientCredentials;
+use rspotify::spotify::senum::{AlbumType, Country};
+
 #[test]
 fn test_album() {
     // Set client_id and client_secret in .env file or
@@ -19,5 +21,65 @@ fn test_album() {
         .build();
     let birdy_uri = "spotify:album:0sNOF9WDwhWunNAHPD3Baj";
     let albums = spotify.album(birdy_uri);
+    assert!(albums.is_ok());
+}
+#[test]
+fn test_albums() {
+    let client_credential = SpotifyClientCredentials::default().build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+    let birdy_uri1 = String::from("spotify:album:41MnTivkwTO3UUJ8DrqEJJ");
+    let birdy_uri2 = String::from("spotify:album:6JWc4iAiJ9FjyK0B59ABb4");
+    let birdy_uri3 = String::from("spotify:album:6UXCm6bOO4gFlDQZV5yL37");
+    let track_uris = vec![birdy_uri1, birdy_uri2, birdy_uri3];
+    let albums = spotify.albums(track_uris);
+    assert!(albums.is_ok())
+}
+#[test]
+fn test_album_tracks() {
+    let client_credential = SpotifyClientCredentials::default().build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+    let birdy_uri = "spotify:album:6akEvsycLGftJxYudPjmqK";
+    let tracks = spotify.album_track(birdy_uri, Some(2), None);
+    assert!(tracks.is_ok());
+}
+
+#[test]
+fn test_artist_related_artists() {
+    let client_credential = SpotifyClientCredentials::default().build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+    let birdy_uri = "spotify:artist:43ZHCT0cAZBISjO8DG9PnE";
+    let artist = spotify.artist_related_artists(birdy_uri);
+    assert!(artist.is_ok())
+}
+
+#[test]
+fn test_artist() {
+    let client_credential = SpotifyClientCredentials::default().build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+    let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
+    let artist = spotify.artist(birdy_uri);
+    assert!(artist.is_ok());
+}
+
+#[test]
+fn test_artists_albums() {
+    let client_credential = SpotifyClientCredentials::default().build();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+    let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
+    let albums = spotify.artist_albums(birdy_uri,
+                                       Some(AlbumType::Album),
+                                       Some(Country::UnitedStates),
+                                       Some(10),
+                                       None);
     assert!(albums.is_ok());
 }
