13e222582d8690379cb2f5ef17369573e0d253bd
Samray <samrayleung@gmail.com>
2018-10-07 12:49:03 +0800

    add SpotifyClientCredentials debug info

--- tests/test_device.rs
+++ tests/test_device.rs
@@ -1,8 +1,8 @@
+extern crate reqwest;
+extern crate rspotify;
 use rspotify::spotify::client::Spotify;
 use rspotify::spotify::oauth2::SpotifyClientCredentials;
 use reqwest::Client;
-use reqwest::Response;
-use mockers::Scenario;
 
 //TODO waitting for mutable mockito test framework
 #[test]#[ignore]
