ef9e0ff9b9476f3554a4e5ce2dc14fa1e7516316
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-08-22 00:51:31 +0200

    Moved docs to docs.rs

--- README.md
+++ README.md
@@ -3,105 +3,17 @@
 [![Crates.io](https://img.shields.io/crates/v/rspotify.svg)](https://crates.io/crates/rspotify)
 [![Docs](https://docs.rs/rspotify/badge.svg)](https://docs.rs/crate/rspotify/)
 
-# Rspotify - a Rust client for The Spotify Web API 
+# Rspotify
 
 Rspotify is a wrapper for the [Spotify Web API](https://developer.spotify.com/web-api/), inspired by [spotipy](https://github.com/plamere/spotipy). It includes support for all the [authorization flows](https://developer.spotify.com/documentation/general/guides/authorization-guide/), and helper functions for [all endpoints](https://developer.spotify.com/documentation/web-api/reference/).
 
-## Usage
+To learn how to use Rspotify, please refer to the [documentation](https://docs.rs/crate/rspotify/). There are some [examples that may be useful](./examples) as well.
 
-Add this to your `Cargo.toml`:
-
-``` toml
-[dependencies]
-rspotify = "0.10.0"
-```
-
-Thanks to [`reqwest`](https://docs.rs/reqwest/0.10.1/reqwest/#proxies), Rspotify supports system proxies by default. `reqwest` reads the environment variables `HTTP_PROXY` and `HTTPS_PROXY` environmental variables to set HTTP and HTTPS proxies, respectively.
-
-## Getting Started
-
-### Authorization
-
-Since all methods require user authorization, you will need to generate a token that indicates that the user has granted permission for your application to perform the given task. You will need to [register your app to get the credentials necessary to make authorized calls](https://developer.spotify.com/dashboard/applications). Read the [official guide for a detailed explanation of the different authorization flows available](https://developer.spotify.com/documentation/general/guides/authorization-guide/).
-
-Even if your script does not have an accessible URL, you will have to specify a redirect URI when registering your application where Spotify will redirect to after a successful login. The URL doesn't need to work or be accessible, you can use "http://localhost/", and an [authorization code](https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow) will be given as the `code` HTTP parameter: "http://localhost/?code=...", which can be used by Rspotify to obtain an access token for your requests. For example:
-
-![demo](./doc/images/rspotify.gif)
-
-In order to help other developers to get used to `rspotify`, I registered a Spotify account with temporary email. You can test `rspotify` with this account's `CLIENT_ID` and `CLIENT_SECRET`, check the [`.env` file](./.env) for more details.
-
-### Examples
-
-There are some [available examples](./examples) which can serve as a learning tool. The following snippet will obtain the top tracks for an artist:
-
-``` toml
-[dependencies]
-rspotify = { version = "0.10.0" }
-tokio = { version = "0.2", features = ["full"] }
-```
-
-``` rust
-use rspotify::client::Spotify;
-use rspotify::oauth2::SpotifyClientCredentials;
-use rspotify::senum::Country;
-
-#[tokio::main]
-async fn main() {
-    // Set client_id and client_secret in .env file or
-    // export CLIENT_ID="your client_id"
-    // export CLIENT_SECRET="secret"
-    let client_credential = SpotifyClientCredentials::default().build();
-
-    // Or set client_id and client_secret explictly
-    // let client_credential = SpotifyClientCredentials::default()
-    //     .client_id("this-is-my-client-id")
-    //     .client_secret("this-is-my-client-secret")
-    //     .build();
-    let spotify = Spotify::default()
-        .client_credentials_manager(client_credential)
-        .build();
-    let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
-    let tracks = spotify
-        .artist_top_tracks(birdy_uri, Country::UnitedStates)
-        .await;
-    println!("{:?}", tracks.unwrap());
-}
-```
-
-### Blocking API example
-
-There is an optional client API that can be enabled for non-asynchronous requests:
-
-``` toml
-[dependencies]
-rspotify = { version = "0.10.0", features = ["blocking"] }
-```
-
-``` rust
-use rspotify::blocking::client::Spotify;
-use rspotify::blocking::oauth2::SpotifyClientCredentials;
-use rspotify::senum::Country;
-
-fn main() {
-    let client_credential = SpotifyClientCredentials::default().build();
-    let spotify = Spotify::default()
-        .client_credentials_manager(client_credential)
-        .build();
-    let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
-    let tracks = spotify.artist_top_tracks(birdy_uri, Country::UnitedStates);
-    println!("{:?}", tracks.unwrap());
-}
-```
-
-## API Documentation
-
-For more API information, check the [Rspotify API documentation](https://docs.rs/crate/rspotify).
-
-## CHANGELOG
+## Changelog
 
 Please see the [changelog](./CHANGELOG.md) for a release history and indications on how to upgrade from one version to another.
 
-## Contribution
+## Contributing
 
 If you find any problems or have suggestions about this crate, please submit an issue. Moreover, any pull request, code review and feedback are welcome.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,118 @@
-//! ## Description
-//! Rspotify is a lightweight wrapper for the [Spotify Web API](https://developer.spotify.com/web-api/) It includes helper functions for
-//! **all Spotify's endpoints**, such as fetching metadata (search and look-up of
-//! albums, artists, tracks, playlists, new releases) and user's information (follow
-//! users, artists and playlists, and saved tracks management).
-//! ## Features
-//! *rspotify* supports all the features of the Spotify Web API including access
-//! to all endpoints, and support for user authorization. Note that before
-//! accessing to any endpoints, you need to be authorized. For details on the
-//! capabilities you are encouraged to review the [Spotify Web Endpoint
-//! Reference](https://developer.spotify.com/web-api/endpoint-reference/)
-//! documentation
+//! Rspotify is a wrapper for the [Spotify Web
+//! API](https://developer.spotify.com/web-api/), inspired by [spotipy
+//! ](https://github.com/plamere/spotipy). It includes support for all the
+//! [authorization flows](https://developer.spotify.com/documentation/general/guides/authorization-guide/),
+//! and helper functions for [all endpoints
+//! ](https://developer.spotify.com/documentation/web-api/reference/).
+//!
+//! ## Configuration
+//!
+//! Add this to your `Cargo.toml`:
+//!
+//! ``` toml
+//! [dependencies]
+//! rspotify = "0.10.0"
+//! ```
+//!
+//! Thanks to [`reqwest`](https://docs.rs/reqwest/0.10.1/reqwest/#proxies),
+//! Rspotify supports system proxies by default. `reqwest` reads the
+//! environment variables `HTTP_PROXY` and `HTTPS_PROXY` environmental
+//! variables to set HTTP and HTTPS proxies, respectively.
 //!
 //! ## Getting Started
+//!
 //! ### Authorization
-//! Since all methods require user authorization now, you will need to
-//! generate an authorization token that indicates that the user has granted
-//! permission for your application to perform the given task. You will need to
-//! register your app to get the credentials necessary to make authorized calls.
 //!
-//! Even if your script does not have an accessible URL you need to specify one when
-//! registering your application where the spotify authentication API will redirect
-//! to after successful login. The URL doesn't need to work or be accessible, you
-//! can specify "http://localhost/", and after a successful login you just need
-//! to copy the "http://localhost/?code=..." URL from your browser and paste it
-//! to the console where your application is running.
+//! Since all methods require user authorization, you will need to generate a
+//! token that indicates that the user has granted permission for your
+//! application to perform the given task. You will need to [register your app
+//! to get the credentials necessary to make authorized calls
+//! ](https://developer.spotify.com/dashboard/applications). Read the
+//! [official guide for a detailed explanation of the different authorization
+//! flows available](https://developer.spotify.com/documentation/general/guides/authorization-guide/).
 //!
-//! ## Examples
-//! If you have a use case you are intertested in, you could check the
-//! [examples](./examples), which has all kinds of detailed examples. For example,
-//! if you want to get recently played history, you could check
-//! [current_user_recently_played](https://github.com/samrayleung/rspotify/blob/master/examples/current_user_recently_played.rs). This is
-//! the example code:
-//! ``` rust
+//! Even if your script does not have an accessible URL, you will have to
+//! specify a redirect URI when registering your application where Spotify
+//! will redirect to after a successful login. The URL doesn't need to work
+//! or be accessible, you can use "http://localhost/", and an [authorization
+//! code](https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow)
+//! will be given as the `code` HTTP parameter: "http://localhost/?code=...",
+//! which can be used by Rspotify to obtain an access token for your requests.
+//! For example:
+//!
+//! ![demo](https://raw.githubusercontent.com/ramsayleung/rspotify/master/doc/images/rspotify.gif)
+//!
+//! In order to help other developers to get used to `rspotify`, I registered
+//! a Spotify account with temporary email. You can test `rspotify` with this
+//! account's `CLIENT_ID` and `CLIENT_SECRET`, check the [`.env`
+//! file](https://github.com/ramsayleung/rspotify/blob/master/.env)
+//! for more details.
+//!
+//! ### Examples
+//!
+//! There are some [available
+//! examples](https://github.com/ramsayleung/rspotify/tree/master/examples)
+//! which can serve as a learning tool. The following snippet will obtain the
+//! top tracks for an artist:
+//!
+//! ```toml
+//! [dependencies]
+//! rspotify = { version = "0.10.0" }
+//! tokio = { version = "0.2", features = ["full"] }
+//! ```
+//!
+//! ```rust
 //! use rspotify::client::Spotify;
-//! use rspotify::util::get_token;
-//! use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
+//! use rspotify::oauth2::SpotifyClientCredentials;
+//! use rspotify::senum::Country;
 //!
 //! #[tokio::main]
 //! async fn main() {
 //!     // Set client_id and client_secret in .env file or
 //!     // export CLIENT_ID="your client_id"
 //!     // export CLIENT_SECRET="secret"
-//!     // export REDIRECT_URI=your-direct-uri
+//!     let client_credential = SpotifyClientCredentials::default().build();
 //!
-//!     // Or set client_id, client_secret,redirect_uri explictly
-//!     // let oauth = SpotifyOAuth::default()
+//!     // Or set client_id and client_secret explictly
+//!     // let client_credential = SpotifyClientCredentials::default()
 //!     //     .client_id("this-is-my-client-id")
 //!     //     .client_secret("this-is-my-client-secret")
-//!     //     .redirect_uri("http://localhost:8888/callback")
 //!     //     .build();
-//!
-//!     let mut oauth = SpotifyOAuth::default()
-//!         .scope("user-read-recently-played")
+//!     let spotify = Spotify::default()
+//!         .client_credentials_manager(client_credential)
 //!         .build();
-//!     match get_token(&mut oauth).await {
-//!         Some(token_info) => {
-//!             let client_credential = SpotifyClientCredentials::default()
-//!                 .token_info(token_info)
-//!                 .build();
-//!             // Or set client_id and client_secret explictly
-//!             // let client_credential = SpotifyClientCredentials::default()
-//!             //     .client_id("this-is-my-client-id")
-//!             //     .client_secret("this-is-my-client-secret")
-//!             //     .build();
-//!             let spotify = Spotify::default()
-//!                 .client_credentials_manager(client_credential)
-//!                 .build();
-//!             let history = spotify.current_user_recently_played(10).await;
-//!             println!("{:?}", history);
-//!         }
-//!         None => println!("auth failed"),
-//!     };
+//!     let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
+//!     let tracks = spotify
+//!         .artist_top_tracks(birdy_uri, Country::UnitedStates)
+//!         .await;
+//!     println!("{:?}", tracks.unwrap());
 //! }
+//! ```
 //!
+//! ### Blocking API example
+//!
+//! There is an optional client API that can be enabled for non-asynchronous
+//! requests:
+//!
+//! ```toml
+//! [dependencies]
+//! rspotify = { version = "0.10.0", features = ["blocking"] }
+//! ```
+//!
+//! ```rust
+//! use rspotify::blocking::client::Spotify;
+//! use rspotify::blocking::oauth2::SpotifyClientCredentials;
+//! use rspotify::senum::Country;
+//!
+//! fn main() {
+//!     let client_credential = SpotifyClientCredentials::default().build();
+//!     let spotify = Spotify::default()
+//!         .client_credentials_manager(client_credential)
+//!         .build();
+//!     let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
+//!     let tracks = spotify.artist_top_tracks(birdy_uri, Country::UnitedStates);
+//!     println!("{:?}", tracks.unwrap());
+//! }
 //! ```
 
 #[cfg(feature = "blocking")]
