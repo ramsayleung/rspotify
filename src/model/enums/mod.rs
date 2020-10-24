b762798785b5eab751bfdfe39edc1ad19148556e
Ramsay <ramsayleung@gmail.com>
2020-10-24 21:04:00 +0800

    Group import path.

--- src/model/enums/mod.rs
+++ src/model/enums/mod.rs
@@ -5,14 +5,6 @@ pub mod types;
 
 pub use country::Country;
 
-pub use misc::DisallowKey;
-pub use misc::IncludeExternal;
-pub use misc::RepeatState;
-pub use misc::TimeRange;
+pub use misc::{DisallowKey, IncludeExternal, RepeatState, TimeRange};
 
-pub use types::AdditionalType;
-pub use types::AlbumType;
-pub use types::CurrentlyPlayingType;
-pub use types::DeviceType;
-pub use types::SearchType;
-pub use types::Type;
+pub use types::{AdditionalType, AlbumType, CurrentlyPlayingType, DeviceType, SearchType, Type};
