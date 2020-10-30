8b2affd4082664989ac63d447218c990c390a170
Ramsay <ramsayleung@gmail.com>
2020-10-30 21:12:03 +0800

    Remove since attribute from deprecated flag.

--- src/model/show.rs
+++ src/model/show.rs
@@ -81,7 +81,6 @@ pub struct SimplifiedEpisode {
     pub is_externally_hosted: bool,
     pub is_playable: bool,
     #[deprecated(
-        since = "0.11",
         note = "This `language` field is deprecated and might be removed in the future by Spotify. Please use the languages field instead"
     )]
     pub language: String,
