f1e6043ac51e14ee3eb7e1a91a4908a2abb90ed9
Ramsay <ramsayleung@gmail.com>
2020-10-31 10:49:40 +0800

    Replace `start`, `duration`, `confidence` fields from `AudioAnalysisSection` and `AudioAnalysisSegment` to `TimeInterval` field.

--- src/model/audio.rs
+++ src/model/audio.rs
@@ -53,12 +53,11 @@ pub struct TimeInterval {
     pub confidence: f32,
 }
 
-/// [audio analysis](https://developer.spotify.com/web-api/get-audio-analysis/)
+/// [Audio section](https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-analysis/#section-object)
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct AudioAnalysisSection {
-    pub start: f32,
-    pub duration: f32,
-    pub confidence: f32,
+    #[serde(flatten)]
+    pub time_interval: TimeInterval,
     pub loudness: f32,
     pub tempo: f32,
     pub tempo_confidence: f32,
@@ -81,12 +80,11 @@ pub struct AudioAnalysisMeta {
     pub analysis_time: f32,
     pub input_process: String,
 }
-///[audio analysis](https://developer.spotify.com/web-api/get-audio-analysis/)
+///[Audio segment](https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-analysis/#segment-object)
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct AudioAnalysisSegment {
-    pub start: f32,
-    pub duration: f32,
-    pub confidence: f32,
+    #[serde(flatten)]
+    pub time_interval: TimeInterval,
     pub loudness_start: f32,
     pub loudness_max_time: f32,
     pub loudness_max: f32,
@@ -124,4 +122,74 @@ pub struct AudioAnalysisTrack {
     pub synch_version: f32,
     pub rhythmstring: String,
     pub rhythm_version: f32,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_audio_analysis_section() {
+        let json_str = r#"
+        {
+            "start": 237.02356,
+            "duration": 18.32542,
+            "confidence": 1,
+            "loudness": -20.074,
+            "tempo": 98.253,
+            "tempo_confidence": 0.767,
+            "key": 5,
+            "key_confidence": 0.327,
+            "mode": 1,
+            "mode_confidence": 0.566,
+            "time_signature": 4,
+            "time_signature_confidence": 1
+        }
+        "#;
+        let session: AudioAnalysisSection = serde_json::from_str(&json_str).unwrap();
+        assert_eq!(session.time_interval.duration, 18.32542);
+    }
+    #[test]
+    fn test_audio_analysis_segments() {
+        let json_str = r#"
+         {
+            "start": 252.15601,
+            "duration": 3.19297,
+            "confidence": 0.522,
+            "loudness_start": -23.356,
+            "loudness_max_time": 0.06971,
+            "loudness_max": -18.121,
+            "loudness_end": -60,
+            "pitches": [
+                0.709,
+                0.092,
+                0.196,
+                0.084,
+                0.352,
+                0.134,
+                0.161,
+                1,
+                0.17,
+                0.161,
+                0.211,
+                0.15
+            ],
+            "timbre": [
+                23.312,
+                -7.374,
+                -45.719,
+                294.874,
+                51.869,
+                -79.384,
+                -89.048,
+                143.322,
+                -4.676,
+                -51.303,
+                -33.274,
+                -19.037
+            ]
+            }
+            "#;
+        let segment: AudioAnalysisSegment = serde_json::from_str(&json_str).unwrap();
+        assert_eq!(segment.time_interval.start, 252.15601);
+    }
 }
