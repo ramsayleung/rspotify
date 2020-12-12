68506bddfcc016b637d79865e17a45604aa8a6c9
Ramsay <ramsayleung@gmail.com>
2020-12-12 16:47:00 +0800

    Add test for test_full_track.

--- tests/test_models.rs
+++ tests/test_models.rs
@@ -494,3 +494,82 @@ fn test_audio_features() {
     let duration = Duration::from_millis(255349);
     assert_eq!(audio_features.duration, duration);
 }
+
+#[test]
+fn test_full_track() {
+    let json = r#"
+    {
+  "album": {
+    "album_type": "single",
+    "artists": [
+      {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/artist/6sFIWsNpZYqfjUpaCgueju"
+        },
+        "href": "https://api.spotify.com/v1/artists/6sFIWsNpZYqfjUpaCgueju",
+        "id": "6sFIWsNpZYqfjUpaCgueju",
+        "name": "Carly Rae Jepsen",
+        "type": "artist",
+        "uri": "spotify:artist:6sFIWsNpZYqfjUpaCgueju"
+      }
+    ],
+    "available_markets": [
+      "ZA"
+    ],
+    "external_urls": {
+      "spotify": "https://open.spotify.com/album/0tGPJ0bkWOUmH7MEOR77qc"
+    },
+    "href": "https://api.spotify.com/v1/albums/0tGPJ0bkWOUmH7MEOR77qc",
+    "id": "0tGPJ0bkWOUmH7MEOR77qc",
+    "images": [
+      {
+        "height": 64,
+        "url": "https://i.scdn.co/image/5a73a056d0af707b4119a883d87285feda543fbb",
+        "width": 64
+      }
+    ],
+    "name": "Cut To The Feeling",
+    "release_date": "2017-05-26",
+    "release_date_precision": "day",
+    "type": "album",
+    "uri": "spotify:album:0tGPJ0bkWOUmH7MEOR77qc"
+  },
+  "artists": [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/6sFIWsNpZYqfjUpaCgueju"
+      },
+      "href": "https://api.spotify.com/v1/artists/6sFIWsNpZYqfjUpaCgueju",
+      "id": "6sFIWsNpZYqfjUpaCgueju",
+      "name": "Carly Rae Jepsen",
+      "type": "artist",
+      "uri": "spotify:artist:6sFIWsNpZYqfjUpaCgueju"
+    }
+  ],
+  "available_markets": [
+    "ZA"
+  ],
+  "disc_number": 1,
+  "duration_ms": 207959,
+  "explicit": false,
+  "external_ids": {
+    "isrc": "USUM71703861"
+  },
+  "external_urls": {
+    "spotify": "https://open.spotify.com/track/11dFghVXANMlKmJXsNCbNl"
+  },
+  "href": "https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl",
+  "id": "11dFghVXANMlKmJXsNCbNl",
+  "is_local": false,
+  "name": "Cut To The Feeling",
+  "popularity": 63,
+  "preview_url": "https://p.scdn.co/mp3-preview/3eb16018c2a700240e9dfb8817b6f2d041f15eb1?cid=774b29d4f13844c495f206cafdad9c86",
+  "track_number": 1,
+  "type": "track",
+  "uri": "spotify:track:11dFghVXANMlKmJXsNCbNl"
+}
+    "#;
+    let full_track: FullTrack = serde_json::from_str(&json).unwrap();
+    let duration = Duration::from_millis(207959);
+    assert_eq!(full_track.duration, duration);
+}
