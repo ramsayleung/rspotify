9284618c18d8277f7bae7db0c222f3e1baa79bc2
Ramsay <ramsayleung@gmail.com>
2021-01-13 23:29:47 +0800

    fix cargo fmt error

--- src/model/enums/misc.rs
+++ src/model/enums/misc.rs
@@ -83,7 +83,7 @@ pub enum RestrictionReason {
 /// Indicates the modality (major or minor) of a track
 /// This field will contain a 0 for `minor`, a 1 for `major` or
 /// a -1 for `no result`
-/// 
+///
 /// [Reference](https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-analysis/#section-object)
 #[derive(Clone, Serialize, Deserialize, Copy, PartialEq, Eq, Debug, ToString)]
 pub enum Modality {
