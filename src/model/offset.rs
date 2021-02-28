9e4adbd071d26510c0c066486692eb9bb139ad04
Konstantin Stepanov <me@kstep.me>
2021-02-28 11:57:21 +0300

    type-safer offset ctor

--- src/model/offset.rs
+++ src/model/offset.rs
@@ -11,15 +11,14 @@ pub enum Offset<T> {
     Uri(IdBuf<T>),
 }
 
-impl<T> Offset<T> {
+impl Offset<idtypes::Track> {
     pub fn for_position(position: u64) -> Offset<idtypes::Track> {
         Offset::Position(Duration::from_millis(position))
     }
+}
 
-    pub fn for_uri(uri: &Id<T>) -> Offset<T>
-    where
-        T: PlayableIdType,
-    {
+impl<T: PlayableTypeId> Offset<T> {
+    pub fn for_uri(uri: &Id<T>) -> Offset<T> {
         Offset::Uri(uri.to_owned())
     }
 }
