5a9ed4ea09d45512751e2f7f5112e20ac80388c7
Rodrigo <rodrigomauricio@protonmail.com>
2021-04-15 12:24:37 -0300

    Updated examples

--- examples/current_user_recently_played.rs
+++ examples/current_user_recently_played.rs
@@ -45,7 +45,7 @@ async fn main() {
     spotify.prompt_for_user_token().await.unwrap();
 
     // Running the requests
-    let history = spotify.current_user_recently_played(10).await;
+    let history = spotify.current_user_recently_played(10, None).await;
 
     println!("Response: {:?}", history);
 }
