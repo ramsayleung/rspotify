fe141d18fda41b4de6e63c84800f373138bc73e7
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-04-17 18:49:38 +0200

    mention to GATs isn't really necessary

--- src/pagination/mod.rs
+++ src/pagination/mod.rs
@@ -1,11 +1,6 @@
 //! Utilities for pagination requests. If the configured client is asynchronous,
 //! it'll be  based on `futures::stream::Stream`, if it's synchronous it'll just
 //! use `std::iter::Iterator`.
-//!
-//! In the future, this may be extended to use the same interface for all
-//! manual, synchronous and asynchronous iteration with a nicer interface. The
-//! problem is that it requires GATs to be implemented. See this comment:
-//! `<https://github.com/ramsayleung/rspotify/pull/166#issuecomment-793698024>`
 
 #[cfg(feature = "__sync")]
 mod iter;
