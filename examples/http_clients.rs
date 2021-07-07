699c4981e5e3066f6e4269198507265d7b165997
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-07-08 01:33:33 +0200

    Some basic examples

---
+++ examples/http_clients.rs
@@ -0,0 +1,23 @@
+use rspotify::{model::Id, prelude::*, ClientCredsSpotify, Credentials, http::{ReqwestClient, UreqClient}};
+
+#[tokio::main]
+async fn main() {
+    // You can use any logger for debugging.
+    env_logger::init();
+
+    let creds = Credentials::from_env().unwrap();
+
+    let mut spotify_reqwest = ClientCredsSpotify::<ReqwestClient>::new(creds);
+    let mut spotify_ureq = ClientCredsSpotify::<UreqClient>::new(creds);
+
+    spotify_reqwest.request_token().await.unwrap();
+    spotify_ureq.request_token().unwrap();
+
+    // Running the requests
+    let birdy_uri = Id::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
+    let albums = spotify_reqwest.album(birdy_uri).await;
+    println!("Response with reqwest: {:#?}", albums);
+
+    let albums = spotify_ureq.album(birdy_uri).await;
+    println!("Response with ureq: {:#?}", albums);
+}
