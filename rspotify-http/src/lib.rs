47f1979180f64c96ad5b826c7dd14f6f10eb797d
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-07-08 01:16:21 +0200

    Clean up rspotify_http

--- rspotify-http/src/lib.rs
+++ rspotify-http/src/lib.rs
@@ -6,38 +6,18 @@
 // confusing errors..
 
 #[cfg(feature = "client-reqwest")]
-#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 mod reqwest;
 
 #[cfg(feature = "client-ureq")]
-#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 mod ureq;
 
 #[cfg(any(feature = "client-reqwest", feature = "client-ureq"))]
-#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 mod common;
 
 #[cfg(feature = "client-reqwest")]
-#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 pub use self::reqwest::ReqwestClient as HttpClient;
 
 #[cfg(feature = "client-ureq")]
-#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 pub use self::ureq::UreqClient as HttpClient;
 
-#[cfg(any(feature = "client-reqwest", feature = "client-ureq"))]
-#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 pub use common::{BaseHttpClient, Form, Headers, HttpError, HttpResult, Query};
-
-#[cfg(all(feature = "client-reqwest", feature = "client-ureq"))]
-compile_error!(
-    "`client-reqwest` and `client-ureq` features cannot both be enabled at \
-    the same time, if you want to use `client-ureq` you need to set \
-    `default-features = false`"
-);
-
-#[cfg(not(any(feature = "client-reqwest", feature = "client-ureq")))]
-compile_error!(
-    "You have to enable at least one of the available clients with the \
-    `client-reqwest` or `client-ureq` features."
-);
