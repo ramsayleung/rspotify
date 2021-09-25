0dbda18c8f81d4e705f736c89cfc51401226a90a
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-09-25 15:05:08 +0200

    Fix threading example

--- examples/ureq/threading.rs
+++ examples/ureq/threading.rs
@@ -1,7 +1,7 @@
 //! This example showcases how the Rspotify client can be used to perform
 //! multithreaded requests as well.
 
-use rspotify::{model::Id, prelude::*, ClientCredsSpotify, Credentials};
+use rspotify::{model::AlbumId, prelude::*, ClientCredsSpotify, Credentials};
 use std::{
     sync::{mpsc::channel, Arc},
     thread,
@@ -12,10 +12,11 @@ fn main() {
 
     let spotify = ClientCredsSpotify::new(creds);
     let ids = [
-        Id::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap(),
-        Id::from_uri("spotify:album:5EBb7SSkPgxO9Lmt8NjAPT").unwrap(),
-        Id::from_uri("spotify:album:3jtOeny1Xh5fp6aSOHahe2").unwrap(),
+        AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap(),
+        AlbumId::from_uri("spotify:album:5EBb7SSkPgxO9Lmt8NjAPT").unwrap(),
+        AlbumId::from_uri("spotify:album:3jtOeny1Xh5fp6aSOHahe2").unwrap(),
     ];
+    let num_ids = ids.len();
     let mut handles = Vec::new();
     let (wr, rd) = channel();
 
@@ -27,14 +28,14 @@ fn main() {
         let spotify = Arc::clone(&spotify);
         let wr = wr.clone();
         let handle = thread::spawn(move || {
-            let albums = spotify.album(id).unwrap();
+            let albums = spotify.album(&id).unwrap();
             wr.send(albums).unwrap();
         });
 
         handles.push(handle);
     }
 
-    for _ in ids {
+    for _ in 0..num_ids {
         let album = rd.recv().unwrap();
         println!("Album: {}", album.name);
     }
