d3341cf87d1a33ceef412b592111074edfa14c36
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-11-19 12:09:10 +0100

    Fix unnecessary references in tests

--- tests/test_with_oauth.rs
+++ tests/test_with_oauth.rs
@@ -96,7 +96,7 @@ pub async fn oauth_client() -> AuthCodeSpotify {
 }
 
 #[maybe_async]
-async fn fetch_all<'a, T>(paginator: Paginator<'a, ClientResult<T>>) -> Vec<T> {
+async fn fetch_all<T>(paginator: Paginator<'_, ClientResult<T>>) -> Vec<T> {
     #[cfg(feature = "__async")]
     {
         use futures::stream::TryStreamExt;
@@ -346,8 +346,8 @@ async fn test_playback() {
         // Starting playback of some songs
         client
             .start_uris_playback(
-                uris.clone(),
-                Some(&device_id),
+                uris,
+                Some(device_id),
                 Some(Offset::for_position(0)),
                 None,
             )
@@ -355,29 +355,29 @@ async fn test_playback() {
             .unwrap();
 
         for i in 0..uris.len() - 1 {
-            client.next_track(Some(&device_id)).await.unwrap();
+            client.next_track(Some(device_id)).await.unwrap();
 
             // Also trying to go to the previous track
             if i != 0 {
-                client.previous_track(Some(&device_id)).await.unwrap();
-                client.next_track(Some(&device_id)).await.unwrap();
+                client.previous_track(Some(device_id)).await.unwrap();
+                client.next_track(Some(device_id)).await.unwrap();
             }
 
             // Making sure pause/resume also works
             let playback = client.current_playback(None, None::<&[_]>).await.unwrap();
             if let Some(playback) = playback {
                 if playback.is_playing {
-                    client.pause_playback(Some(&device_id)).await.unwrap();
+                    client.pause_playback(Some(device_id)).await.unwrap();
                     client.resume_playback(None, None).await.unwrap();
                 } else {
                     client.resume_playback(None, None).await.unwrap();
-                    client.pause_playback(Some(&device_id)).await.unwrap();
+                    client.pause_playback(Some(device_id)).await.unwrap();
                 }
             }
         }
 
         client
-            .transfer_playback(&next_device_id, Some(true))
+            .transfer_playback(next_device_id, Some(true))
             .await
             .unwrap();
     }
