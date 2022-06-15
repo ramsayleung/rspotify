3a016c951f9c074764508cdb00d6b0604e1b64ef
SabrinaJewson <sejewson@gmail.com>
2022-06-15 16:21:34 +0100

    Explain why iterators aren't used in `build_map`

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -5,6 +5,8 @@ use std::collections::HashMap;
 pub(crate) fn build_map<'key, 'value, const N: usize>(
     array: [(&'key str, Option<&'value str>); N],
 ) -> HashMap<&'key str, &'value str> {
+    // Use a manual for loop instead of iterators so we can call `with_capacity` and avoid
+    // reallocating.
     let mut map = HashMap::with_capacity(N);
     for (key, value) in array {
         if let Some(value) = value {
