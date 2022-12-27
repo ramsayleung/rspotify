c5d595097019046474a1661b86a8d37abc7670fd
Mario Ortiz Manero <marioortizmanero@gmail.com>
2022-12-27 14:38:45 +0100

    Use more `chrono::Duration`

--- rspotify-model/src/offset.rs
+++ rspotify-model/src/offset.rs
@@ -1,8 +1,10 @@
 //! Offset object
 
+use chrono::Duration;
+
 /// Offset object
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Offset {
-    Position(u32),
+    Position(Duration),
     Uri(String),
 }
