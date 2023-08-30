f4425b67325d2520af20dfcb83cdf23a1c567481
Thomas Ung <tomkimsour@gmail.com>
2023-08-30 18:49:34 +0200

    move non oauth function to base.rs

--- src/clients/base.rs
+++ src/clients/base.rs
@@ -637,6 +637,24 @@ where
         convert_result(&result)
     }
 
+    /// Get cover image of a playlist.
+    ///
+    /// Parameters:
+    /// - playlist_id - the playlist ID, URI or URL
+    /// [reference](https://developer.spotify.com/documentation/web-api/reference/get-playlist-cover)
+    async fn playlist_cover_image(
+        &self,
+        playlist_id: PlaylistId<'_>,
+    ) -> ClientResult<Option<Image>> {
+        let url = format!("playlists/{}/images", playlist_id);
+        let result = self.api_get(&url, &Query::new()).await?;
+        if result.is_empty() {
+            Ok(None)
+        } else {
+            convert_result(&result)
+        }
+    }
+
     /// Get Spotify catalog information for a single show identified by its unique Spotify ID.
     ///
     /// Path Parameters:
