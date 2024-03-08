cd55e43f30fc25dd90aadec5cfa408eb6b833825
Ramsay Leung <ramsayleung@gmail.com>
2024-03-08 00:25:49 -0800

    Fix cargo clippy error.

--- examples/ureq/seek_track.rs
+++ examples/ureq/seek_track.rs
@@ -16,7 +16,7 @@ fn main() {
     // This function requires the `cli` feature enabled.
     spotify.prompt_for_token(&url).unwrap();
 
-    match spotify.seek_track(chrono::Duration::seconds(25), None) {
+    match spotify.seek_track(chrono::Duration::try_seconds(25).unwrap(), None) {
         Ok(_) => println!("Change to previous playback successful"),
         Err(_) => eprintln!("Change to previous playback failed"),
     }
